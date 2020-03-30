b750057925f284aade7e5c022fb3bd56421803e3
Alan Rocha <alan.rocha@udem.edu>
2020-03-29 18:09:53 -0600

    Added load function and feature scaling

--- utilityfunctions.py
+++ utilityfunctions.py
@@ -2,7 +2,8 @@
 
 import numpy as np
 import pandas as pd
-
+import matplotlib.pyplot as plt
+import time
 
 def eval_hypothesis_function(w, x):
     """Evaluate the hypothesis function"""
@@ -36,29 +37,66 @@ def compute_L2_norm(gradient_of_cost_function):
     """compute L2-norm of gradient of cost function"""
     return np.sqrt(np.sum(gradient_of_cost_function**2))
 
+def feature_scaling(data):
+    """ standarize the x data and saves mean value & std value"""
+    mean_value = data.mean()
+    std_value =  data.std()
+    scaling_scores = (data - mean_value) / std_value
+    return scaling_scores, mean_value, std_value
 
 def load_data(path_and_filename):
     """ load data from comma-separated-value (CSV) file """
 
     # load training-data file
-    training_data = pd.read_csv('training-data.csv')
+    training_data = pd.read_csv(path_and_filename)
 
-    # retrieve x data from training data; this is converted from Pandas.Series to
-    # numpy data
-    x_data = pd.Series.to_numpy(training_data['x'])
+    # retrieve x data from training data; this is converted from Pandas.DataFrame
+    #to numpy
+    n_rows, n_columns = training_data.shape
 
+    x_data = pd.DataFrame.to_numpy(training_data.iloc[:,0:n_columns-1])
+
+    # retrieve y data from training data; this is converted from Pandas.DataFrame
+    # to numpy
+
+    y = pd.DataFrame.to_numpy(training_data.iloc[:,-1]).reshape(n_rows, 1)
+
+    # prints x and y data 
+    print("--"*23)
+    print("Training Data Outputs")
+    print("--"*23)
+    for x,y in zip(x_data,y):
+        print(x,y)
+
+    # creates list to save mean, std and scaled x values
+    new_scaled_x = []
+    mean_list = []
+    std_list = []
+
+    # scales features
+    for feature in x_data.T:
+        new_data, mean_value, std_value = feature_scaling(feature)
+        new_scaled_x.append(new_data)
+        mean_list.append(mean_value)
+        std_list.append(std_value)
+    
+    # Transpose to columns
+    x_data = np.array(new_scaled_x).T
+    print("\n")
+    print("--"*23)
+    print("Training Data Scaled")
+    print("--"*23)
+    for x,y in zip(x_data, x_data):
+        print(x)
+    
     # create a ones-vector of size as x
-    one_data = 1
+    one_data = np.ones(shape=n_rows)
 
-    # concatenate one_data with x_data vertically
-    x = np.vstack((np.ones(len(x_data)),x_data))
-
-    # retrieve labels from training data; these are converted from Pandas.Series
-    # to numpy data
-    y = pd.Series.to_numpy(training_data['y'])
-
+    # concatenate one_data with x_data
+    x = np.column_stack((x_data,one_data))
+    
     # return x and y data
-    return x,y
+    return x, y, mean_list, std_list
 
 
 def gradient_descent(x_training, y_training, w, stopping_criteria, learning_rate):
